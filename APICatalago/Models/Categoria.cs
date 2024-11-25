dc5940cecfdb99df266751e4ba48f2a3f4d11eed
franchesco aprigio <franchescodsa@outlook.com>
2024-11-25 20:23:08 -0300

    Criandoo modelo de dominio

---
+++ APICatalago/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalago.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemgUrl { get; set; }
+    }
+}

7023df56fbafc812048ce74e8f8926cdfefdb959
franchesco aprigio <franchescodsa@outlook.com>
2024-11-25 23:42:08 -0300

    Criando aclasse de contexto doEntity F core

---
+++ APICatalago/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using APICatalago.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalago.Context
+{
+    // realizar a comunucação entre as  entidades eo banco de dados
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}

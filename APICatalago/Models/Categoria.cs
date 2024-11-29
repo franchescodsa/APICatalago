4e8108b494ed3ff39c24127ebf2dd7fc3b8e42ed
franchesco aprigio <franchescodsa@outlook.com>
2024-11-28 21:54:59 -0300

    Aplicando Data Annotations

--- APICatalago/Models/Categoria.cs
+++ APICatalago/Models/Categoria.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.VisualBasic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalago.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria()
         {
             Produtos = new Collection<Produto>();
         }
+        [Key] //Mapeando como chave primaria
         public int CategoriaId { get; set; }
+        [Required]// coluna vai not null
+        [StringLength(80)] // DEFININDO O TAMANHO DA STRING NO BD
         public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImagemgUrl { get; set; }
         //Relacionamento um-para-muitos. Ctaregoria é uma chave estrangeito de produto. 
         public ICollection<Produto>? Produtos { get; set; }
